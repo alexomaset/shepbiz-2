1a241ec506e4a42e38e149c4e57115488ed48b91
alexomaset <alexomaset711@gmail.com>
2023-06-01 14:14:49 +0300

    add log chages

--- pages/blog-standard.js
+++ pages/blog-standard.js
@@ -34,34 +34,6 @@ export default function Home({ posts }) {
             <BlogCard key={post.sys.id} post={post} />
           ))}
         </div>
-        <div className="col-xl-4 col-lg-5">
-          <div className="sidebar-widget-area">
-            <div className="widget search-widget mb-40 wow fadeInUp">
-              <form onSubmit={(e) => e.preventDefault()}>
-                <div className="form_group">
-                  <input
-                    type="email"
-                    className="form_control"
-                    placeholder="Search here"
-                    name="email"
-                    required=""
-                  />
-                  <button className="search-btn">
-                    <i className="far fa-search" />
-                  </button>
-                </div>
-              </form>
-            </div>
-            <div className="widget tag-cloud-widget mb-40 wow fadeInUp">
-              <h4 className="widget-title">Popular Tags</h4>
-              <a href="#">Avocado</a>
-              <a href="#">Groundnuts</a>
-              <a href="#">Cashew Nuts</a>
-              <a href="#">Macadamia</a>
-              <a href="#">Milled Rice</a>
-            </div>
-          </div>
-        </div>
       </section>
     </Layout>
   );
